f83ff2b8ff314b71be0fed6b5833e6a7cb41c28f
Hugodiazz <hugozaid2002@gmail.com>
2023-11-14 11:25:21 -0600

    Update
    Sex

--- src/main/java/View/Escena1.java
+++ src/main/java/View/Escena1.java
@@ -1,12 +1,14 @@
 package View;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Escena1 {
@@ -15,28 +17,37 @@ public class Escena1 {
     private Label titulo;
     private VBox contenedorPrincipal;
     private Stage stage;
+    private HBox contenedorTitulo;
 
     public Escena1(Stage stage) {
         this.stage = stage;
         contenedorPrincipal = new VBox();
+        contenedorPrincipal.setId("ColorFondoNegro");
+        contenedorTitulo = new HBox();
         titulo = new Label("OnyxTube");
-        titulo.setAlignment(Pos.CENTER);
+        titulo.setId("EstiloTituloEscena1");
+        contenedorTitulo.getChildren().add(titulo);
+        contenedorTitulo.setPadding(new Insets(15));
         scrollPane = createScrollPane(lista);
-        contenedorPrincipal.getChildren().addAll(titulo, scrollPane);
+        contenedorPrincipal.getChildren().addAll(contenedorTitulo, scrollPane);
 
         Scene scene = new Scene(contenedorPrincipal, 800, 600);
-        stage.setTitle("Escena 1");
         stage.setScene(scene);
-        stage.show();
+
+        String cssPath = "/Estilos.css"; // Reemplaza con la ruta correcta
+        scene.getStylesheets().add(cssPath);
     }
 
     public VBox createScrollPane(String[] content) {
-        VBox container = new VBox();
+        VBox container = new VBox(10);
+        container.setPadding(new Insets(0,40,0,40));
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setFitToWidth(true);
+        scrollPane.setId("scroll-pane");
         HBox labelsContainer = new HBox(12);
 
         Label tituloV = new Label("Acción");
+        tituloV.setId("EstiloGeneroEscena1");
 
         for (String s : content) {
             Label text = new Label(s);
